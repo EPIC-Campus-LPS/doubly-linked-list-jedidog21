5580ba06ecfc4989fc769f0cec215b46b6bddd36
benf <jedidoh21@gmail.com>
2026-02-11 14:23:25 -0700

    make work

---
+++ DoublyLinkedListTest.java
@@ -0,0 +1,38 @@
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class DoublyLinkedListTest {
+//    @org.junit.jupiter.api.Test
+//    void add() {
+//        DoublyLinkedList <Integer> doubly = new DoublyLinkedList<Integer>();
+//        doubly.add(10);
+//        assertEquals(10, doubly.remove());
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void testAdd() {
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void remove() {
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void testRemove() {
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void get() {
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void set() {
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void size() {
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void isEmpty() {
+//    }
+//}
